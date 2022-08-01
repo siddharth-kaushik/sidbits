e898a1e33c5ba35be72866ab3c28d1414ea99f58
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-08-01 17:11:20 +0530

    add Logo component

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <span className="group font-bold select-none">
+      Sidbits&nbsp;
+      <span className="opacity-0 group-hover:opacity-50 transition-opacity">
+        Inc.
+      </span>
+    </span>
+  )
+}
+
+export default Logo
