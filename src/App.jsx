8fa3c66e60e9aaad3ab2237e51ff406661178878
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-08-01 17:12:30 +0530

    update app to use Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import { Header } from './components'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-      <span>Sidbits Inc.</span>
-      <span className="font-mono text-xs text-gray-400">2015-2022&nbsp;</span>
-      <span className="text-sm">unless otherwise noted.</span>
+    <div className="antialiased text-stone-800 dark:text-stone-400 bg-white dark:bg-stone-900">
+      <Header />
     </div>
   )
 }
