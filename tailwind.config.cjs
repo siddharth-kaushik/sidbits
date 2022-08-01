61d35c7237e970b624f2f34dcf4ce9f01693e00f
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-08-01 17:10:17 +0530

    add display font to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       sans: ['apercu', ...defaultTheme.fontFamily.sans],
       serif: ['p22-mackinac-pro', ...defaultTheme.fontFamily.serif],
       mono: ['apercu-mono', ...defaultTheme.fontFamily.mono],
+      display: ['fino', defaultTheme.fontFamily.display],
     },
     extend: {},
   },
