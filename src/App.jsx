9d2ca8d6743bc2bfba5d479a92caa1bd38ba8eb3
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-08-01 16:35:57 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl">Welcome</h1>
+      <span>Sidbits Inc.</span>
+      <span className="font-mono text-xs text-gray-400">2015-2022&nbsp;</span>
+      <span className="text-sm">unless otherwise noted.</span>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['apercu', ...defaultTheme.fontFamily.sans],
+      serif: ['p22-mackinac-pro', ...defaultTheme.fontFamily.serif],
+      mono: ['apercu-mono', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
