b1046cbe204ae7c888641583489c031b5a2b75c3
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-08-02 10:16:59 +0530

    update logo styles

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <span className="group text-lg font-bold select-none flex items-center gap-1">
+    <span className="group text-lg dark:text-zinc-200 font-bold select-none flex items-center gap-1">
       Sidbits
-      <span className="opacity-0 group-hover:opacity-50 transition-opacity">
+      <span className="opacity-0 dark:opacity-50 group-hover:opacity-50 transition-opacity">
         Inc.
       </span>
     </span>
