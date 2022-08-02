c241f7277d46300194e324cbe5bd2d562f4f2309
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-08-02 10:15:47 +0530

    update app to include outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from 'react-router-dom'
 import { Header } from './components'
 
 function App() {
   return (
-    <div>
+    <div className="h-full grid grid-rows-[max-content_1fr_max-content]">
       <Header />
+      <main className="w-full max-w-7xl mx-auto">
+        <Outlet />
+      </main>
     </div>
   )
 }
